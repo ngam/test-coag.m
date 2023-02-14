4f279b0372bc9757322e055619c1c9bca2175118
ngam <67342040+ngam@users.noreply.github.com>
2023-02-14 11:11:25 -0500

    test

---
+++ code/calcCoag.m
@@ -0,0 +1,3 @@
+function coag_out = testCoag(coag_in)
+    coag_out = coag_in.^2;
+end
---
+++ test/TestCoag.m
@@ -0,0 +1,11 @@
+classdef TestCoag < matlab.unittest.TestCase
+    methods (Test)
+        function testCoag(testCase)
+            addpath('../code')
+            coag_in = 11;
+            coag_out = calcCoag(coag_in);
+            coag_out_truth = coag_out .^ 2;
+            testCase.verifyEqual(coag_out, coag_out_truth);
+        end
+    end
+end
